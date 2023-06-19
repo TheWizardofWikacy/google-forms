d965bfeb52c4c9060515a1d8b7b9ba5a577d2d5e
Encountermeasure <62707165+TheWizardofWikacy@users.noreply.github.com>
2023-06-19 13:30:58 -0400

    Update authService.js
    Added a section to easily replace with firebase auth.

--- src/services/authService.js
+++ src/services/authService.js
@@ -72,3 +72,95 @@ export default   {
        return jwtDecode(localStorage.getItem('userTicket'));
      },
   };
+
+//or, use firebase auth
+/*
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import jwtDecode from 'jwt-decode';
+import jwt from 'jsonwebtoken';
+
+const firebaseConfig = {
+  // Add your Firebase config here
+  // apiKey: "YOUR_API_KEY",
+  // authDomain: "YOUR_AUTH_DOMAIN",
+  // projectId: "YOUR_PROJECT_ID",
+  // ...
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default {
+  isAuthenticated() {
+    const user = firebase.auth().currentUser;
+    return user !== null;
+  },
+
+  getGuestUser() {
+    return { name: 'Guest 123', userId: 'guest123', email: 'coolboy69@gg.com' };
+  },
+
+  authenticate(cb) {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const data = {
+          name: displayName,
+          email: email,
+          image: photoURL,
+        };
+        return axios.post(API_URL + 'login', data);
+      })
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.accessToken) {
+          localStorage.setItem('userTicket', JSON.stringify(response.data.accessToken));
+        }
+        cb();
+      })
+      .catch((error) => {
+        console.log(error);
+        cb(error);
+      });
+  },
+
+  signout(cb) {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        localStorage.removeItem('userTicket');
+        cb();
+      })
+      .catch((error) => {
+        console.log(error);
+        cb(error);
+      });
+  },
+
+  loginAsGuest() {
+    var userData = {
+      name: 'Cool Guest',
+      id: 'y2jsdqakq9rqyvtd4gf6g',
+      email: 'coolboy69@gg.com',
+    };
+
+    const accessToken = jwt.sign(userData, 'thisisaguesttokenwithsomeshittystring8', { expiresIn: '24h' });
+    localStorage.setItem('userTicket', JSON.stringify(accessToken));
+    return accessToken;
+  },
+
+  logout() {
+    firebase.auth().signOut();
+    localStorage.removeItem('userTicket');
+  },
+
+  getCurrentUser() {
+    return jwtDecode(localStorage.getItem('userTicket'));
+  },
+};
+
+*/
